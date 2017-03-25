2f67cb9437ecc43ba57516fccdf89730dcd5ac8c
Mithweth <jeanbaptiste.langlois@gmail.com>
2017-03-25 07:38:44 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "python-ansible",
+    version = "0.0.1",
+    author = "Jean-Baptiste LANGLOIS",
+    author_email = jeanbaptiste.langlois@gmail.com",
+    license = "GPL",
+    packages=['pyansible']
+)
